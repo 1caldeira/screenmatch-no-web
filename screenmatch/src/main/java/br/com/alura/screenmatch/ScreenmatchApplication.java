59a8b551a52bdb4f15bd451c70a05c80e92b169e
1caldeira <g.caldeiraa1@gmail.com>
2023-12-07 22:16:06 -0300

    First commit

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DataConversion;
+import br.com.alura.screenmatch.model.SeriesData;
+import br.com.alura.screenmatch.service.RequestAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var requestAPI = new RequestAPI();
+		var json = requestAPI.getData("https://www.omdbapi.com/?t=house+of+the+dragon&apikey=ad0f5b1d");
+
+		DataConversion conversion = new DataConversion();
+		SeriesData data = conversion.getData(json, SeriesData.class);
+		System.out.println(data);
+		System.out.println("git commit");
+
+	}
+}
